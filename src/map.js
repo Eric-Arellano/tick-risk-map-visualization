5bf13e69fb98d22a080c74c0aec49f3c4194bad0
Eric-Arellano <ericarellano@me.com>
2016-10-14 14:14:00 -0500

    add initial searchbox to map

--- src/map.js
+++ src/map.js
@@ -1,8 +1,16 @@
-var jaliscoCoast = {lat: 19.67, lng: -104.67};
 var map;
+
 function initMap() {
+    var jaliscoCoast = new google.maps.LatLng(19.67, -104.67);
+
     map = new google.maps.Map(document.getElementById('map'), {
         center: jaliscoCoast,
         zoom: 9
     });
+
+    // set up search box
+    var input = document.getElementById('location-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
 }
